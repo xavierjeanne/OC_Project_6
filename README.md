3498bd247ee66704271fa772572d7ea8d9679491
xjeanne <x.jeanne@102-103.com>
2025-05-15 13:52:15 +0200

    algorithme brute force

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # OC_Project_6
+Ce projet fait partie du cours de développement Python d'OpenClassrooms. Le but de ce projet est de résoudre des problèmes en utlisant des algorithmes en Python.
+
+## Description
+Concevoir un algorithme qui maximisera le profit réalisé par les clients après deux ans d'investissement.
+Les contraintes sont les suivantes :    
+- Le budget total ne doit pas dépasser 500€ / client.
+- Impossible d'acheter des fractions d'action.
+- Chaque action ne peut être acheté qu'une fois.
+
+le programme essaie toutes les différentes combinaisons d'actions qui correspondent aux contraintes, et choisit le meilleur résultat. Le programme doit donc lire un fichier contenant des informations sur les actions, explorer toutes les combinaisons possibles et afficher le meilleur investissement.
+
+Dans un premier temps, le programme utilise la méthode brute force pour trouver la meilleure combinaison d'actions. La méthode brute force consiste à explorer toutes les combinaisons possibles d'actions et à choisir la meilleure combinaison qui correspond aux contraintes.
+
+## Prérequis
+
+- Python 3.x
+- Git
+
+## Installation
+
+1. Clonez ce dépôt :
+   Dans un terminal, exécuter la commande suivante : 
+   git clone https://github.com/xavierjeanne/OC_Project_6.git
+
+2. Accéder au répertoire du projet :
+   cd Oc_project_6
+
+   
+3. Créer un environnement virtuel, l'activer puis installer les dépendances :
+   python -m venv env
+   source env\Scripts\activate
+   pip install -r requirements.txt
+
+4. Executer le script :
+   python brute-force.py
---
+++ brute-force.py
@@ -0,0 +1,87 @@
+import csv
+from itertools import combinations
+
+
+def read_stocks(file_path):
+    """Reads the stocks from the file.
+
+    Args:
+        file_path (str): Path to the CSV file.
+
+    Returns:
+        list: List of stocks.
+    """
+    stocks = []
+    with open(file_path, "r") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            name = row[0]
+            try:
+                price = float(row[1])
+                profit_percentage = float(row[2].strip('%'))/100
+                stocks.append({
+                    "name": name,
+                    "price": price,
+                    "profit_percentage": profit_percentage
+                })
+            except (ValueError, IndexError):
+                print(f"Erreur lors de la lecture de la ligne: {row}")
+    return stocks
+
+
+def calculate_profit(combination):
+    """ Calculate the total profit for a combination of stocks.
+
+        Args:
+            combination (list): List of stocks in the combination.
+
+        Returns:
+            tuple: Total cost and total profit.
+    """
+
+    total_cost = sum(stock['price'] for stock in combination)
+    total_profit = sum(stock['price'] * stock['profit_percentage']
+                       for stock in combination)
+
+    return total_cost, total_profit
+
+
+def find_best_combination(stocks, max_budget=500):
+    """ Find the best combination of stocks using brute force.
+
+        Args:
+            stocks (list): List of stocks.
+            max_budget (float): Maximum budget for the combination.
+
+        Returns:
+            tuple: Best combination of stocks, total cost, and total profit.
+    """
+    best_combination = []
+    best_profit = 0
+    best_cost = 0
+
+    # Try all possible combinations of stocks
+    for i in range(1, len(stocks) + 1):
+        for combo in combinations(stocks, i):
+            cost, profit = calculate_profit(combo)
+            if cost <= max_budget and profit > best_profit:
+                best_combination = combo
+                best_profit = profit
+                best_cost = cost
+    return best_combination, best_cost, best_profit
+
+
+if __name__ == "__main__":
+
+    file_path = "dataset/action.csv"
+    stocks = read_stocks(file_path)
+
+    best_combination, best_cost, best_profit = find_best_combination(stocks)
+
+    print("Meilleure combinaison d'actions:")
+    for stock in best_combination:
+        print(f"{stock['name']}: {stock['price']} €, {stock['profit_percentage'] * 100:.2f}%")
+
+    print(f"Coût total: {best_cost} €")
+    print(f"Bénéfice total: {best_profit} €")
